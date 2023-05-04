7db186a26144c80826b6c035579613e220535b27
Reinaldo <reinaldopared@gmail.com>
2023-05-04 17:22:01 -0400

    update gitpy

--- gitpy.py
+++ gitpy.py
@@ -1,4 +1,18 @@
+import argparse
+import collections
+import difflib
+import enum
+import hashlib
+import operator
 import os
+import stat
+import struct
+import sys
+import time
+import urllib.request
+import zlib
+
+# Initializing a repo
 
 
 def init(repo):
@@ -10,6 +24,8 @@ def init(repo):
     write_file(os.path.join(repo, '.git', 'HEAD'),
                b'ref: refs/heads/master')
     print('initialized empty repository: {}'.format(repo))
+
+# Hashing objects
 
 
 def hash_object(data, obj_type, write=True):
@@ -24,3 +40,132 @@ def hash_object(data, obj_type, write=True):
         if not os.path.exists(path):
             write_file(path, zlib.compress(full_data))
     return sha1
+
+# The git index
+
+
+# Data for one entry in the git index (.git/index)
+IndexEntry = collections.namedtuple('IndexEntry', [
+    'ctime_s', 'ctime_n', 'mtime_s', 'mtime_n', 'dev', 'ino', 'mode',
+    'uid', 'gid', 'size', 'sha1', 'flags', 'path',
+])
+
+
+def read_index():
+    """Read git index file nd return list of IndexEntry objects."""
+    try:
+        data = read_file(os.path.join('.git', 'index'))
+    except FileNotFoundError:
+        return []
+    digest = hashlib.sha1(data[:-20]).digest()
+    assert digest == data[-20:], 'invalid index checksum'
+    signature, version, num_entries = struct.unpack('!4sLL', data[:12])
+    assert signature == b'DIRC', \
+        'invalid index signature {}'.format(signature)
+    assert version == 2, 'unknown index version {}'.format(version)
+    entry_data = data[12:-20]
+    entries = []
+    i = 0
+    while i + 62 < len(entry_data):
+        fields_end = i + 62
+        fields = struct.unpack('!LLLLLLLLLL20sH',
+                               entry_data[i:fields_end])
+        path_end = entry_data.index(b'\x00', fields_end)
+        path = entry_data[fields_end:path_end]
+        entry = IndexEntry(*(fields + (path.decode(),)))
+        entries.append(entry)
+        entry_len = ((62 + len(path) + 8) // 8) * 8
+        i += entry_len
+    assert len(entries) == num_entries
+    return entries
+
+# Commit
+# Tree object commit
+
+
+def write_tree():
+    """Write a tree object from the current index entries."""
+    tree_entries = []
+    for entry in read_index():
+        assert '/' not in entry.path, \
+            'currently only supports a single, top-level directory'
+        mode_path = '{:o} {}'.format(entry.mode, entry.path).encode()
+        tree_entry = mode_path + b'\x00' + entry.sha1
+        tree_entries.append(tree_entry)
+    return hash_object(b''.join(tree_entries), 'tree')
+
+# Commit as object
+
+
+def commit(message, author):
+    """Commit the current state of the index to master with given message.
+    Return hash of commit object.
+    """
+    tree = write_tree()
+    parent = get_local_master_hash()
+    timestamp = int(time.mktime(time.localtime()))
+    utc_offset = -time.timezone
+    author_time = '{} {}{:02}{:02}'.format(
+        timestamp,
+        '+' if utc_offset > 0 else '-',
+        abs(utc_offset) // 3600,
+        (abs(utc_offset) // 60) % 60)
+    lines = ['tree' + tree]
+    if parent:
+        lines.append('parent ' + parent)
+    lines.append('author {} {}'.format(author, author_time))
+    lines.append('committer {} {}'.format(author, author_time))
+    lines.append('')
+    lines.append(message)
+    lines.append('')
+    data = '\n'.join(lines).encode()
+    sha1 = hash_object(data, 'commit')
+    master_path = os.path.join('.git', 'refs', 'heads', 'master')
+    write_file(master_path, (sha1 + '\n').encode())
+    print('committed to master: {:7}'.format(sha1))
+    return sha1
+
+# Talking to a server
+# The pkt-line format
+
+
+def extract_lines(data):
+    """Extract list of lines from given server data."""
+    lines = []
+    i = 0
+    for _ in range(1000):
+        line_length = int(data[i:i + 4], 16)
+        line = data[i + 4:i + line_length]
+        lines.append(line)
+        if line_length == 0:
+            i += 4
+        else:
+            i += line_length
+        if i >= len(data):
+            break
+    return lines
+
+
+def build_lines_data(lines):
+    """Build byte string from given lines to send to server."""
+    result = []
+    for line in lines:
+        result.append('{:04x}'.format(len(line) + 5).encode())
+        result.append(line)
+        result.append(b'\n')
+    result.append(b'0000')
+    return b''.join(result)
+
+# Making an HTTPS request
+
+
+def http_request(url, username, password, data=None):
+    """Make an authenticated HTTP request to given URL (GET by default,
+    POST if "data" is not None).
+    """
+    password_manager = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+    password_manager.add_password(None, url, username, password)
+    auth_handler = urllib.request.HTTPBasicAuthHandler(password_manager)
+    opener = urllib.request.build_opener(auth_handler)
+    f = opener.open(url, data=data)
+    return f.read()
